b31b796209e3152310e3f7b8d61ad1e472799fa8
Ascetic <398121373@qq.com>
2021-08-04 21:53:06 +0800

    feat: daily leetcode

--- javascript/leetcode__0021_merge_two_sorted_lists.js
+++ javascript/leetcode__0021_merge_two_sorted_lists.js
@@ -1,36 +1,73 @@
 /**
- * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的
  * 
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * 
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * 
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
  */
 
 'use strict'
 
 const assert = require('assert');
 
-function solution (parentheses) {
-    const stack = [];
+const { LinkNode, LinkedList } = require('./classical/structure/doubly-linked-list');
 
-    const compare = (prev, next) => (prev === '(' && next === ')') || (prev === '[' && next === ']') || (prev === '{' && next === '}');
+function solution (linkedList1, linkedList2) {
+    if (linkedList1.head && linkedList2.head) {
+        let min = linkedList1.head.data >= linkedList2.head.data ? linkedList2.head : linkedList1.head;
+        let max = min === linkedList2.head ? linkedList1.head : linkedList2.head;
+        const head = min === linkedList2.head ? linkedList2 : linkedList1;
 
-    for (let i = 0; i < parentheses.length; i++) {
-        if (!stack.length) {
-            stack.unshift(parentheses[i])
-            continue;
+        while (min) {
+            if (min.next && (min.next.data <= max.data)) {
+                min = min.next;
+                continue;
+            }
+            const next = min.next;
+            min.next = max;
+            min = next;
+            [min, max] = [max, min];
         }
-        if (compare(stack[0], parentheses[i])) {
-            stack.shift()
-        } else {
-            stack.unshift(parentheses[i])
-        }
+        return head;
+    } else {
+        return linkedList1.head ? linkedList1 : linkedList2
     }
-    return !stack.length
 }
 
 
 (function validParentheses () {
-    assert.strictEqual(solution('()'), true)
-    assert.strictEqual(solution('()[]{}'), true)
-    assert.strictEqual(solution('(]'), false)
-    assert.strictEqual(solution('([)]'), false)
-    assert.strictEqual(solution('{[]}'), true)
+    const linkedList1 = new LinkedList();
+    const linkedList2 = new LinkedList();
+    const linkedList3 = new LinkedList();
+    const linkedList4 = new LinkedList();
+
+    linkedList1
+        .add(new LinkNode(1), null)
+        .add(new LinkNode(2), new LinkNode(1))
+        .add(new LinkNode(4), new LinkNode(2));
+
+    linkedList2
+        .add(new LinkNode(1), null)
+        .add(new LinkNode(3), new LinkNode(1))
+        .add(new LinkNode(4), new LinkNode(3));
+
+    linkedList3
+        .add(new LinkNode(1), null)
+        .add(new LinkNode(2), new LinkNode(1))
+        .add(new LinkNode(9), new LinkNode(2));
+
+    linkedList4
+        .add(new LinkNode(4), null)
+        .add(new LinkNode(5), new LinkNode(4))
+        .add(new LinkNode(6), new LinkNode(5));
+
+    assert.deepEqual(solution(linkedList1, linkedList2).toArray(), [1, 1, 2, 3, 4, 4])
+    assert.deepEqual(solution(linkedList3, linkedList4).toArray(), [1, 2, 4, 5, 6, 9])
+    assert.deepEqual(solution(new LinkedList(), new LinkedList()).toArray(), [])
+    assert.deepEqual(solution(new LinkedList(), new LinkedList().add(new LinkNode(1), null)).toArray(), [1])
 })()
