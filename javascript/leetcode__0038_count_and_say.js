c516c9f802d98b61a80db1cfea226d888628b4ea
Ascetic <398121373@qq.com>
2022-06-25 08:33:32 +0800

    wip: daily leetcode

---
+++ javascript/leetcode__0038_count_and_say.js
@@ -0,0 +1,32 @@
+/**
+ * 给定一个正整数 n ，输出外观数列的第 n 项。
+ * 「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。
+ * 你可以将其视作是由递归公式定义的数字字符串序列
+ *      countAndSay(1) = "1"
+ *      countAndSay(n) 是对 countAndSay(n-1) 的描述，然后转换成另一个数字字符串
+ * 
+ * 输入：n = 1
+ * 输出："1"
+ * 解释：这是一个基本样例
+ * 
+ * 
+ * 输入：n = 4
+ * 输出："1211"
+ * 解释：
+ * countAndSay(1) = "1"
+ * countAndSay(2) = 读 "1" = 一 个 1 = "11"
+ * countAndSay(3) = 读 "11" = 二 个 1 = "21"
+ * countAndSay(4) = 读 "21" = 一 个 2 + 一 个 1 = "12" + "11" = "1211"
+ * 
+ */
+
+ 'use strict'
+
+ const assert = require('assert');
+ 
+ function solution (n) {
+ }
+ 
+ 
+ (function countAndSay () {
+ })()
